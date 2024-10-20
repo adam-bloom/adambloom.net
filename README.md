c2589bcf47b07a2a3400827f38a288e15ca580fe
Adam Bloom <adam-bloom@users.noreply.github.com>
2024-10-20 13:19:48 -0600

    create new astro project from template

---
+++ README.md
@@ -0,0 +1,7 @@
+# Adam Bloom's Personal Website
+
+Using [AstroWind](https://github.com/onwidget/astrowind)
+
+## Acknowledgements
+
+Theme initially created by [onWidget](https://onwidget.com) and maintained by a community of [contributors](https://github.com/onwidget/astrowind/graphs/contributors).
---
+++ src/navigation.ts
@@ -0,0 +1,12 @@
+export const footerData = {
+  socialLinks: [
+    {
+      ariaLabel: 'Facebook',
+      icon: 'tabler:brand-linkedin',
+      href: 'https://linkedin.com/in/bloomadams',
+      target: '_blank',
+    },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/adam-bloom', target: '_blank' },
+    { ariaLabel: 'Email', icon: 'tabler:mail', href: 'mailto:hello@adambloom.net' },
+  ],
+};
